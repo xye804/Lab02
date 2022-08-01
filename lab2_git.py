7867af10df0bc723a57b46e45e5ba1e52083c09e
xye804 <xye804@aucklanduni.ac.nz>
2022-08-02 00:35:25 +0800

    implements function toss_coin()

--- lab2_git.py
+++ lab2_git.py
@@ -82,7 +82,11 @@ def toss_coin():
     """TODO: Add your code here
         Hint: Using random.randint() function.
     """
-    coin = 'Head'
+    rand = random.randint(0, 1)
+    if rand == 0:
+        coin = 'Head'
+    else:
+        coin = 'Tail'
     return coin
 
 
